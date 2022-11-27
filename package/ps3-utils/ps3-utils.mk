18cc10934a9cce55ff4ea629b0c41c52ff5bf867
kth5 <alex.bldck@gmail.com>
2022-11-27 20:55:38 +0100

    package/ps3-utils: fix up header files

--- package/ps3-utils/ps3-utils.mk
+++ package/ps3-utils/ps3-utils.mk
@@ -8,10 +8,19 @@ PS3_UTILS_VERSION = c9d3b1279857e546f34233520d679c979b4af8ac
 PS3_UTILS_SITE = git://git.kernel.org/pub/scm/linux/kernel/git/geoff/ps3-utils.git
 PS3_UTILS_LICENSE = GPLv2
 PS3_UTILS_LICENSE_FILES = COPYING
+PS3_UTILS_INSTALL_STAGING = YES
 
 PS3_UTILS_AUTORECONF = YES
 
 PS3_UTILS_CONF_OPTS += \
 	PACKAGE_VERSION=g$(PS3_UTILS_VERSION)
+
+define PS3_UTILS_POST_INSTALL
+	$(INSTALL) -D -m 0755 $(@D)/lib/ps3-flash.h \
+		$(STAGING_DIR)/usr/include/ps3-flash.h
+	$(INSTALL) -D -m 0755 $(@D)/lib/ps3-av.h \
+		$(STAGING_DIR)/usr/include/ps3-av.h
+endef
+PS3_UTILS_POST_INSTALL_TARGET_HOOKS += PS3_UTILS_POST_INSTALL
 
 $(eval $(autotools-package))
